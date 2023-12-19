6be2421f6b6511f42be6ee8b1f3c91315e6bc429
mingsun <msun@ritchiebros.com>
2023-12-19 11:18:50 +0800

    init project

---
+++ src/main/java/com/example/sqlscript/SqlScriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlScriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqlscript/SqlScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
